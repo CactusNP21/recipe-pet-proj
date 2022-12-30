ed0b0b88e5f12470de5c34159deb3ffffc90ebab
CactusNP21 <nazarkucher3212@gmail.com>
2022-12-30 21:59:33 +0200

    initial proj

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,30 +1,42 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {mocked, MockedDishes} from "../../core/mockedDishes";
 import {PageEvent} from "@angular/material/paginator";
 import {SearchService} from "./services/search.service";
-import {BehaviorSubject, Subject} from "rxjs";
+import {BehaviorSubject, Subject, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
-export class MainComponent  {
+export class MainComponent implements OnInit, OnDestroy{
   dishesSubject = new Subject<MockedDishes[]>();
   dishes$ = this.dishesSubject.asObservable();
   pageItems: number;
   pageIndex: number;
   pageSize: number = 5;
-  dishes: MockedDishes[]
+  dishes: MockedDishes[] = []
+  sub: Subscription
   constructor(private search: SearchService) {
-    search.getDishes().subscribe(dishes => {
+    this.sub = search.getDishes().subscribe(dishes => {
       console.log(dishes)
       this.dishes = dishes
       this.pageItems = dishes.length
+      console.log(dishes.slice(0, this.pageSize))
       this.dishesSubject.next(dishes.slice(0, this.pageSize))
     })
   }
 
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.search.search()
+    }, 0)
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe()
+  }
 
   changePage(event: PageEvent) {
     this.pageIndex = event.pageIndex
